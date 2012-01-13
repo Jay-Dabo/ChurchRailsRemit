1235a161b57652ce04c661526b11fbbe05f4a960
Gerald Oki <geraldoki@yahoo.com>
2012-01-13 13:34:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChurchRailsRemit::Application.config.secret_token = '68e7da2812420b3a447aab5719dd5519bec18b3fca945294ca6821dbedd1962955d654ae12dbb3673d9e709f468f630c38a8f70a224b3e26594f83a86da3e012'
